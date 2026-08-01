1baa85890afe22e43aaa99dd61c9078f2c68815d
Holylaw7 <1957298196@qq.com>
2026-08-01 22:10:03 +0800

    fix(payment): add no-args constructor to CreatePayResponse

--- payment-service/src/main/java/com/seckill/payment/dto/CreatePayResponse.java
+++ payment-service/src/main/java/com/seckill/payment/dto/CreatePayResponse.java
@@ -2,10 +2,12 @@ package com.seckill.payment.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CreatePayResponse {
 
